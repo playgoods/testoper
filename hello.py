f14dafe55eb1d15e93bfc8c5e266f6a16ea9f3fe
playgoods <mohmed_e@hotmail.com>
2015-06-08 02:00:54 +0300

    change hello.py add new funciotn userand add file in templates folder user.html

--- hello.py
+++ hello.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/user/<name>')
+def users(name):
+	return render_template("user.html",name=name)
+
 if __name__=="__main__":
 	app.run(debug=True)
